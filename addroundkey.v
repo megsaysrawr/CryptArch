2810b7d3260ee0267063ac2018168168829e081f
Selina Wang <ziyu.wang@students.olin.edu>
2015-12-06 22:53:50 -0500

    AddRoundKey Completed

--- addroundkey.v
+++ addroundkey.v
@@ -4,10 +4,10 @@
 
 module addroundkey
 (
-input 	    clk,            
-input	    shiftrowsout,   // Output from shift rows
-input	    roundkey,
-output reg  addroundkeyout  // Conditioned output signal
+input 	    	 clk,            
+input [7:0]	 shiftrowsout [3:0] [3:0],   // Output from shift rows
+input [7:0] 	 roundkey [3:0] [3:0],
+output reg [7:0] addroundkeyout [3:0] [3:0] // Conditioned output signal
 );
 
 assign addroundkeyout = shiftrowsout ^ roundkey;
