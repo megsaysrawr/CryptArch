b94cf91ecb44ad91ba40cfa6e77b4190080ec904
Meg McCauley <megan@mccauley.net>
2015-12-08 16:56:50 -0500

    EDIT: diffusion.t.v and .v

--- diffusion.t.v
+++ diffusion.t.v
@@ -15,7 +15,7 @@ module diffusion_test_bench_harness();
 	diffusion DUT
 	(	
 		.diffusion_in(diffusion_in),
-		.diffusion_out(diffusion_out),
+		.diffusion_out(diffusion_out)
 		// .srows_out(srows_out)
 	);
 
@@ -25,7 +25,7 @@ module diffusion_test_bench_harness();
 		.endtest(endtest),
 		.dutpassed(dutpassed),
 		.diffusion_in(diffusion_in),
-		.diffusion_out(diffusion_out),
+		.diffusion_out(diffusion_out)
 		// .srows_out(srows_out)
 	);
 
@@ -47,7 +47,7 @@ module diffusion_test_bench(
 	output reg endtest,
 	output reg dutpassed,
 	output reg [7:0] diffusion_in [3:0][3:0],
-	input [7:0] diffusion_out [3:0][3:0],
+	input [7:0] diffusion_out [3:0][3:0]
 	// input [7:0] srows_out [3:0][3:0]
 );
 
@@ -57,7 +57,7 @@ module diffusion_test_bench(
 		dutpassed = 1;
 		#20
 
-		//test case 0--shift rows check with decimal numbers
+		//test case 0--check to see if what is inputted gets stored properly
 		$display("---------------------------------");
 		$display("Testing case 0...");
 		diffusion_in[3][3:0] = {8'd1, 8'd2, 8'd3, 8'd4};
@@ -72,22 +72,9 @@ module diffusion_test_bench(
 		$display("%d %d %d %d", diffusion_in[1][3], diffusion_in[1][2], diffusion_in[1][1], diffusion_in[1][0]);
 		$display("%d %d %d %d", diffusion_in[0][3], diffusion_in[0][2], diffusion_in[0][1], diffusion_in[0][0]);
 
-		// $display("diffusion_out");
-		// $display("%d %d %d %d", diffusion_out[3][3], diffusion_out[3][2], diffusion_out[3][1], diffusion_out[3][0]);
-		// $display("%d %d %d %d", diffusion_out[2][3], diffusion_out[2][2], diffusion_out[2][1], diffusion_out[2][0]);
-		// $display("%d %d %d %d", diffusion_out[1][3], diffusion_out[1][2], diffusion_out[1][1], diffusion_out[1][0]);
-		// $display("%d %d %d %d", diffusion_out[0][3], diffusion_out[0][2], diffusion_out[0][1], diffusion_out[0][0]);
-
-		$display("srows_out");
-		$display("%d %d %d %d", srows_out[3][3], srows_out[3][2], srows_out[3][1], srows_out[3][0]);
-		$display("%d %d %d %d", srows_out[2][3], srows_out[2][2], srows_out[2][1], srows_out[2][0]);
-		$display("%d %d %d %d", srows_out[1][3], srows_out[1][2], srows_out[1][1], srows_out[1][0]);
-		$display("%d %d %d %d", srows_out[0][3], srows_out[0][2], srows_out[0][1], srows_out[0][0]);
-		#20
-
 		//NOTE: yes, this works, but we need to code the if statement check to set dutPassed
 
-		//test case 1--test mix columns using the example given in the cartoon
+		//test case 1--test diffusion out using the example given in the cartoon
 		$display("Testing case 1...");
 		diffusion_in[3][3:0] = {8'hc9, 8'hfb, 8'h92, 8'hf5};
 		diffusion_in[2][3:0] = {8'haf, 8'hda, 8'haa, 8'h6b};
@@ -96,23 +83,16 @@ module diffusion_test_bench(
 		#20
 
 		$display("diffusion_in");
-		$display("%h %h %h %h", diffusion_in[3][3], diffusion_in[3][2], diffusion_in[3][1], diffusion_in[3][0]);
-		$display("%h %h %h %h", diffusion_in[2][3], diffusion_in[2][2], diffusion_in[2][1], diffusion_in[2][0]);
-		$display("%h %h %h %h", diffusion_in[1][3], diffusion_in[1][2], diffusion_in[1][1], diffusion_in[1][0]);
-		$display("%h %h %h %h", diffusion_in[0][3], diffusion_in[0][2], diffusion_in[0][1], diffusion_in[0][0]);
+		$display("%d %d %d %d", diffusion_in[3][3], diffusion_in[3][2], diffusion_in[3][1], diffusion_in[3][0]);
+		$display("%d %d %d %d", diffusion_in[2][3], diffusion_in[2][2], diffusion_in[2][1], diffusion_in[2][0]);
+		$display("%d %d %d %d", diffusion_in[1][3], diffusion_in[1][2], diffusion_in[1][1], diffusion_in[1][0]);
+		$display("%d %d %d %d", diffusion_in[0][3], diffusion_in[0][2], diffusion_in[0][1], diffusion_in[0][0]);
 
-		$display("srows_out");
-		$display("%h %h %h %h", srows_out[3][3], srows_out[3][2], srows_out[3][1], srows_out[3][0]);
-		$display("%h %h %h %h", srows_out[2][3], srows_out[2][2], srows_out[2][1], srows_out[2][0]);
-		$display("%h %h %h %h", srows_out[1][3], srows_out[1][2], srows_out[1][1], srows_out[1][0]);
-		$display("%h %h %h %h", srows_out[0][3], srows_out[0][2], srows_out[0][1], srows_out[0][0]);
-		#20
-
-		$display("mix_columns/diffusion_out");
-		$display("%h %h %h %h", diffusion_out[3][3], diffusion_out[3][2], diffusion_out[3][1], diffusion_out[3][0]);
-		$display("%h %h %h %h", diffusion_out[2][3], diffusion_out[2][2], diffusion_out[2][1], diffusion_out[2][0]);
-		$display("%h %h %h %h", diffusion_out[1][3], diffusion_out[1][2], diffusion_out[1][1], diffusion_out[1][0]);
-		$display("%h %h %h %h", diffusion_out[0][3], diffusion_out[0][2], diffusion_out[0][1], diffusion_out[0][0]);
+		$display("diffusion_out");
+		$display("%d %d %d %d", diffusion_out[3][3], diffusion_out[3][2], diffusion_out[3][1], diffusion_out[3][0]);
+		$display("%d %d %d %d", diffusion_out[2][3], diffusion_out[2][2], diffusion_out[2][1], diffusion_out[2][0]);
+		$display("%d %d %d %d", diffusion_out[1][3], diffusion_out[1][2], diffusion_out[1][1], diffusion_out[1][0]);
+		$display("%d %d %d %d", diffusion_out[0][3], diffusion_out[0][2], diffusion_out[0][1], diffusion_out[0][0]);
 	#5
 	endtest = 1;
 	end
