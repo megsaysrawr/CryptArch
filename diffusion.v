ba21c9c2e0863d45d396e3651c0c4da97eba05ec
Meg McCauley <megan@mccauley.net>
2015-12-10 12:33:18 -0500

    EDIT: Diffusion is done and works properly!!

--- diffusion.v
+++ diffusion.v
@@ -7,6 +7,8 @@ All of this code implies that the matrix is constructed [row, column].
 i.e. [3][3:0] will give you all of the top row and [3:0][3] will give you all of the first column
 */
 
+`include "mix_cols.v"
+
 module shift_rows(
 	input	[7:0] shift_rows_in [3:0][3:0],			//square matrix with 8 bits in each element; width is four, depth is four
 	output	[7:0] shift_rows_out [3:0][3:0]
@@ -27,49 +29,6 @@ module shift_rows(
 
 endmodule
 
-module mix_columns(
-	input	[7:0] mix_col_in [3:0][3:0],
-	output	[7:0] mix_col_out [3:0][3:0]
-);
-	//each column gets its bits mixed up and then spit back out
-	reg [7:0] mixer [3:0][3:0];
-
-	//filling in the mixer matrix row by row (can also be done column by column or element by element if needed)
-	//row 1
-	assign mixer [3][3:0] = {8'd2, 8'd1, 8'd1, 8'd3};
-
-	//row 2
-	assign mixer [2][3:0] = {8'd3, 8'd2, 8'd1, 8'd1};
-
-	//row 3
-	assign mixer [1][3:0] = {8'd1, 8'd3, 8'd2, 8'd1};
-
-	//row 4
-	assign mixer [0][3:0] = {8'd1, 8'd1, 8'd3, 8'd2};
-
-	//matrix multiplication the linearity way--by hand: one element = add each element in the row times each element in the column
-	//first column
-	assign mix_col_out [3][3] = mixer[3][3] * mix_col_in[3][3] + mixer[3][2] * mix_col_in[2][3] + mixer[3][1] * mix_col_in[1][3] + mixer[3][0] * mix_col_in[0][3];
-	assign mix_col_out [2][3] = mixer[2][3] * mix_col_in[3][3] + mixer[2][2] * mix_col_in[2][3] + mixer[2][1] * mix_col_in[1][3] + mixer[2][0] * mix_col_in[0][3];
-	assign mix_col_out [1][3] = mixer[1][3] * mix_col_in[3][3] + mixer[1][2] * mix_col_in[2][3] + mixer[1][1] * mix_col_in[1][3] + mixer[1][0] * mix_col_in[0][3];
-	assign mix_col_out [0][3] = mixer[0][3] * mix_col_in[3][3] + mixer[0][2] * mix_col_in[2][3] + mixer[0][1] * mix_col_in[1][3] + mixer[0][0] * mix_col_in[0][3];
-	//second column
-	assign mix_col_out [3][2] = mixer[3][3] * mix_col_in[3][2] + mixer[3][2] * mix_col_in[2][2] + mixer[3][1] * mix_col_in[1][2] + mixer[3][0] * mix_col_in[0][2];
-	assign mix_col_out [2][2] = mixer[2][3] * mix_col_in[3][2] + mixer[2][2] * mix_col_in[2][2] + mixer[2][1] * mix_col_in[1][2] + mixer[2][0] * mix_col_in[0][2];
-	assign mix_col_out [1][2] = mixer[1][3] * mix_col_in[3][2] + mixer[1][2] * mix_col_in[2][2] + mixer[1][1] * mix_col_in[1][2] + mixer[1][0] * mix_col_in[0][2];
-	assign mix_col_out [0][2] = mixer[0][3] * mix_col_in[3][2] + mixer[0][2] * mix_col_in[2][2] + mixer[0][1] * mix_col_in[1][2] + mixer[0][0] * mix_col_in[0][2];
-	//third column
-	assign mix_col_out [3][1] = mixer[3][3] * mix_col_in[3][1] + mixer[3][2] * mix_col_in[2][1] + mixer[3][1] * mix_col_in[1][1] + mixer[3][0] * mix_col_in[0][1];
-	assign mix_col_out [2][1] = mixer[2][3] * mix_col_in[3][1] + mixer[2][2] * mix_col_in[2][1] + mixer[2][1] * mix_col_in[1][1] + mixer[2][0] * mix_col_in[0][1];
-	assign mix_col_out [1][1] = mixer[1][3] * mix_col_in[3][1] + mixer[1][2] * mix_col_in[2][1] + mixer[1][1] * mix_col_in[1][1] + mixer[1][0] * mix_col_in[0][1];
-	assign mix_col_out [0][1] = mixer[0][3] * mix_col_in[3][1] + mixer[0][2] * mix_col_in[2][1] + mixer[0][1] * mix_col_in[1][1] + mixer[0][0] * mix_col_in[0][1];
-	//fourth column
-	assign mix_col_out [3][0] = mixer[3][3] * mix_col_in[3][0] + mixer[3][2] * mix_col_in[2][0] + mixer[3][1] * mix_col_in[1][0] + mixer[3][0] * mix_col_in[0][0];
-	assign mix_col_out [2][0] = mixer[2][3] * mix_col_in[3][0] + mixer[2][2] * mix_col_in[2][0] + mixer[2][1] * mix_col_in[1][0] + mixer[2][0] * mix_col_in[0][0];
-	assign mix_col_out [1][0] = mixer[1][3] * mix_col_in[3][0] + mixer[1][2] * mix_col_in[2][0] + mixer[1][1] * mix_col_in[1][0] + mixer[1][0] * mix_col_in[0][0];
-	assign mix_col_out [0][0] = mixer[0][3] * mix_col_in[3][0] + mixer[0][2] * mix_col_in[2][0] + mixer[0][1] * mix_col_in[1][0] + mixer[0][0] * mix_col_in[0][0];
-endmodule
-
 module diffusion(
 	input	[7:0] diffusion_in [3:0][3:0],			//square matrix with 8 bits in each element; width is four, depth is four
 	// wire	[7:0] srows_out [3:0][3:0],
@@ -77,11 +36,33 @@ module diffusion(
 );
 	// reg [7:0] srows_out [3:0][3:0];
 	wire	[7:0] srows_out [3:0][3:0];
+	wire	[31:0] col_1_in;
+	wire	[31:0] col_1_out;
+	wire	[31:0] col_2_in;
+	wire	[31:0] col_2_out;
+	wire	[31:0] col_3_in;
+	wire	[31:0] col_3_out;
+	wire	[31:0] col_4_in;
+	wire	[31:0] col_4_out;
+
 	//make a shift_rows module
 	shift_rows srows (.shift_rows_in(diffusion_in),
 						.shift_rows_out(srows_out));
 
+	//create temporary column wires from shift rows out
+	assign col_1_in = {srows_out[3][3], srows_out[2][3], srows_out[1][3], srows_out[0][3]};
+	assign col_2_in = {srows_out[3][2], srows_out[2][2], srows_out[1][2], srows_out[0][2]};
+	assign col_3_in = {srows_out[3][1], srows_out[2][1], srows_out[1][1], srows_out[0][1]};
+	assign col_4_in = {srows_out[3][0], srows_out[2][0], srows_out[1][0], srows_out[0][0]};
+
 	//make a mix_columns module
-	mix_columns mcols (.mix_col_in(srows_out),
-						.mix_col_out(diffusion_out));
+	mix_cols col1 (.input_col(col_1_in), .final_col(col_1_out));
+	mix_cols col2 (.input_col(col_2_in), .final_col(col_2_out));
+	mix_cols col3 (.input_col(col_3_in), .final_col(col_3_out));
+	mix_cols col4 (.input_col(col_4_in), .final_col(col_4_out));
+	assign diffusion_out[3][3:0] = {col_1_out[31:24], col_2_out[31:24], col_3_out[31:24], col_4_out[31:24]};
+	assign diffusion_out[2][3:0] = {col_1_out[23:16], col_2_out[23:16], col_3_out[23:16], col_4_out[23:16]};
+	assign diffusion_out[1][3:0] = {col_1_out[15:8], col_2_out[15:8], col_3_out[15:8], col_4_out[15:8]};
+	assign diffusion_out[0][3:0] = {col_1_out[7:0], col_2_out[7:0], col_3_out[7:0], col_4_out[7:0]};
+
 endmodule
